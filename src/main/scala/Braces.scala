410e5f597714b0083a3c6129251aa8b355cb8355
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2019-02-12 19:19:53 +0300

    some fixes

--- src/main/scala/Braces.scala
+++ src/main/scala/Braces.scala
@@ -1,14 +1,11 @@
-package example
-
-class Braces {
+object Braces {
 
   import scala.collection.mutable
-  import scala.io.StdIn
 
   val left = List("[", "{", "(")
   val right = List("]", "}", ")")
 
-  def z(line: String): Int = {
+  def findError(line: String): Int = {
     val stack = mutable.Stack[String]()
 
     for (i <- 0 until line.length) {
@@ -36,22 +33,6 @@ class Braces {
       line.indexOf(stack.pop) + 1
     }
   }
-
-  def aga(): Unit = {
-    val line = StdIn.readLine()
-    if (line.isEmpty) {
-      println(0)
-      return
-    }
-    val result = z(line)
-    if (result == -1) {
-      println("Success")
-    } else {
-      println(result)
-    }
-  }
-
-  aga()
 }
 
 object Main {
@@ -59,15 +40,16 @@ object Main {
   import scala.io.StdIn
 
   def main(args: Array[String]): Unit = {
-    val stack = new MaxStack()
-
-    for (_ <- 0 until StdIn.readInt) {
-      val line = StdIn.readLine
-      line match {
-        case "pop" => stack.pop()
-        case "max" => println(stack.max)
-        case s if s.startsWith("push") => stack.push(s.stripPrefix("push ").toInt)
-      }
+    val line = StdIn.readLine()
+    if (line.isEmpty) {
+      println(0)
+      return
+    }
+    val result = Braces.findError(line)
+    if (result == -1) {
+      println("Success")
+    } else {
+      println(result)
     }
   }
 }
