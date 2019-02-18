c922c98c68622afabe4eaf55977621f0ce970f5d
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2019-02-18 20:01:32 +0300

    working on MinHeap

--- src/main/scala/MinHeap.scala
+++ src/main/scala/MinHeap.scala
@@ -1,29 +1,27 @@
+import scala.collection.mutable
+
 class MinHeap(heap: Seq[Int]) {
+  private val indexes = mutable.ArrayBuffer[(Int, Int)]()
 
-}
+  def liftUp(heap: Seq[Int], index: Int): Seq[Int] = {
+    if (index - 1 < 0) return heap
 
-object MinHeap {
-  private class Node(val value: Int, var left: Node = null, var right: Node = null)
-
-  def makeHeap(heap: Seq[Int], parent:Node): Node ={
-    heap.length match {
-      case 0 => _
-      case 1 => parent.left = new Node(heap.head)
-      case 2 =>
-        parent.left = new Node(heap.head)
-        parent.right = new Node(heap(1))
-      case _ =>
-        parent.left = {
-          val newLeft = new Node(heap.head)
-          newLeft.left = makeHeap(heap.slice(1, heap.length), newLeft)
-          newLeft.right = makeHeap(heap.slice(2, heap.length), newLeft)
-          new Node (heap.head, makeHeap(heap))
-        }
-    }
-    parent
+    val parentIndex = (index - 1) / 2
+    liftUp(
+      if (heap(parentIndex) > heap(index)) {
+        indexes += ((parentIndex, index))
+        heap
+          .updated(parentIndex, heap(index))
+          .updated(index, heap(parentIndex))
+      } else {
+        heap
+      }, index - 1)
   }
 
   def countPermutations(heap: Seq[Int]): Seq[(Int, Int)] = {
-
+    for (index <- (heap.length - 1) until 0 by 1) {
+      liftUp(heap, index)
+    }
+    indexes
   }
 }
